3368c2c6e1fa3512353b5e4d83306cf92807c78b
Dan Chui <dchui108@gmail.com>
2024-03-21 23:08:38 +0900

    Refactor Scripts.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var navlist = document.getElementById("navbar");
-var sticky = navlist.offsetTop;
+const navlist = document.getElementById('navbar');
+const sticky = navlist.offsetTop;
 
 /* Function to stick the nav bar */
 function myFunction() {
   if (window.scrollY >= sticky) {
-    navlist.classList.add("sticky");
+    navlist.classList.add('sticky');
   } else {
-    navlist.classList.remove("sticky");
+    navlist.classList.remove('sticky');
   }
 }
 
@@ -15,14 +15,14 @@ window.onscroll = function () {
 };
 
 // Toggle Menu - Show or Hide
-var navLinks = document.getElementById("navLinks");
+const navLinks = document.getElementById('navLinks');
 
 // Open the sidenav
 function showMenu() {
-  document.getElementById("navLinks").style.width = "70%";
+  document.getElementById('navLinks').style.width = '70%';
 }
 
 // Close/hide the sidenav
 function hideMenu() {
-  document.getElementById("navLinks").style.width = "0";
+  document.getElementById('navLinks').style.width = '0';
 }
